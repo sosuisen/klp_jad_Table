6ed3c48d4cdb9fc0bd98c6307e08c6760e291392
sosuisen <hidekazu.kubota@gmail.com>
2023-03-24 18:28:00 +0900

    first commit

---
+++ src/main/java/com/example/MyAppController.java
@@ -0,0 +1,193 @@
+package com.example;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+
+public class MyAppController {
+	final String dataPath = "./tododata.json";
+
+	final String TODO_COMPLETED = "完了";
+	final String TODO_TITLE = "タイトル";
+	final String TODO_DATE = "日付";
+	final LinkedHashMap<String, Integer> MENU = new LinkedHashMap<String, Integer>() {{
+		put(TODO_COMPLETED, 0);
+		put(TODO_TITLE, 1);
+		put(TODO_DATE, 2);
+	}};
+	final String SORT_ASCENDANT = "昇順";
+	final String SORT_DESCENDANT = "降順";
+	
+	private ArrayList<ToDo> todos = new ArrayList<>() {
+		// Use instance initializer of anonymouse class
+		{
+			add(new ToDo(1, "Design", "2022-12-01", true));
+			add(new ToDo(2, "Implementation", "2022-12-07", false));
+		}
+	};
+	
+    @FXML
+    private Button addBtn;
+
+    @FXML
+    private DatePicker datePicker;
+
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    private MenuItem menuItemClose;
+
+    @FXML
+    private ChoiceBox<String> sortOrderMenu;
+
+    @FXML
+    private ChoiceBox<String> sortTypeMenu;
+
+    @FXML
+    private TextField titleField;
+
+    @FXML
+    private VBox todoListVBox;
+
+	private ObservableList<Node> todoListItems;
+	
+	private HBox createToDoHBox(ToDo todo) {
+		var completedCheckBox = new CheckBox();
+		completedCheckBox.setSelected(todo.isCompleted());
+		completedCheckBox.getStyleClass().add("todo-completed");
+		completedCheckBox.setOnAction(e -> {
+			System.out.println("チェック更新[" + todo.getId() +"] " + completedCheckBox.isSelected());
+		});
+
+		var titleField = new TextField(todo.getTitle());
+		titleField.getStyleClass().add("todo-title");
+		HBox.setHgrow(titleField, Priority.ALWAYS);
+		titleField.setOnAction(e -> {
+			System.out.println("タイトル更新[" + todo.getId() +"] " + titleField.getText());
+		});
+		titleField.focusedProperty().addListener((observable, oldProperty, newProperty) -> {
+			if (!newProperty) {
+				System.out.println("タイトル更新[" + todo.getId() +"] " + titleField.getText());
+			}
+		});
+
+		var datePicker = new DatePicker(todo.getLocalDate());
+		datePicker.getStyleClass().add("todo-date");
+		datePicker.setPrefWidth(105);
+		HBox.setHgrow(datePicker, Priority.NEVER);
+		datePicker.setOnAction(e -> {
+			System.out.println("日付更新[" + todo.getId() +"] " + datePicker.getValue().toString());
+		});
+		
+		var deleteBtn = new Button("削除");
+		deleteBtn.getStyleClass().add("todo-delete");
+
+		var todoItem = new HBox(completedCheckBox, titleField, datePicker, deleteBtn);
+		todoItem.getStyleClass().add("todo-item");
+
+		deleteBtn.setOnAction(e -> {
+			System.out.println("削除[" + todo.getId() +"]");
+			todoListItems.remove(todoItem);
+		});
+
+		return todoItem;
+	}
+    
+	private ToDo create(String title, String date) {
+		int newId = todos.stream().max((todo1, todo2) -> todo1.getId() - todo2.getId()).get().getId() + 1;
+		var newToDo = new ToDo(newId, title, date, false);
+		todos.add(newToDo);
+
+		return newToDo;
+	}
+	
+	private void showInfo(String txt) {
+		Alert dialog = new Alert(AlertType.INFORMATION);
+		dialog.setHeaderText(null);
+		dialog.setContentText(txt);
+		dialog.showAndWait();
+	}
+
+	private void sort(String type, String order) {
+		Comparator<Node> comp = null;
+		switch(type){
+		case TODO_COMPLETED:
+			comp = Comparator.comparing(node -> ((CheckBox)((HBox)node).getChildren().get(MENU.get(TODO_COMPLETED))).isSelected());
+			break;
+		case TODO_TITLE:
+			comp = Comparator.comparing(node -> ((TextField)((HBox)node).getChildren().get(MENU.get(TODO_TITLE))).getText());
+			break;
+		case TODO_DATE:
+		default:
+			comp = Comparator.comparing(node -> ((DatePicker)((HBox)node).getChildren().get(MENU.get(TODO_DATE))).getValue());
+			break;
+		}
+		if (order.equals(SORT_DESCENDANT)) {
+			comp = comp.reversed();
+		}
+		FXCollections.sort(todoListItems, comp);
+	}
+
+	
+    public void initialize() {
+		sortTypeMenu.getItems().addAll(MENU.keySet());
+		sortTypeMenu.setValue(TODO_DATE);
+		sortTypeMenu.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> sort(newValue, sortOrderMenu.getValue()));
+		
+		sortOrderMenu.getItems().addAll(SORT_ASCENDANT, SORT_DESCENDANT);
+		sortOrderMenu.setValue(SORT_ASCENDANT);
+		sortOrderMenu.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> sort(sortTypeMenu.getValue(), newValue));
+
+
+		// Set today
+		datePicker.setValue(LocalDate.now());
+
+		todoListItems = todoListVBox.getChildren();
+
+		todos.stream()
+		.sorted(Comparator.comparing(ToDo::getDate))
+		.forEach(todo -> {
+			todoListItems.add(createToDoHBox(todo));
+		});
+
+		EventHandler<ActionEvent> handler = e -> {
+			var title = titleField.getText();
+			if (title.equals(""))
+				return;
+			LocalDate localDate = datePicker.getValue(); // 2022-12-01
+			ToDo newToDo = create(title, localDate.toString());
+			todoListItems.add(createToDoHBox(newToDo));
+			sort(sortTypeMenu.getValue(), sortOrderMenu.getValue());
+			titleField.setText("");
+			System.out.println("追加[" + newToDo.getId() +"] " + title);
+		};
+		titleField.setOnAction(handler);
+		addBtn.setOnAction(handler);
+		
+		menuItemAbout.setOnAction(e -> showInfo("ToDo App"));
+		
+		menuItemClose.setOnAction(e -> 	Platform.exit());
+    }
+
+}
